bd327593535a1108ac99d62c0074144520f1d4d1
Harsh <harshmanitripathi08@gmail.com>
2026-09-05 22:25:32 +0530

    mode change to night for default final_8

--- Experience/Theme.js
+++ Experience/Theme.js
@@ -4,20 +4,12 @@ export default class Theme extends EventEmitter{
   constructor() {
     super();
     
-    this.theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
+    this.theme = "dark";
 
     this.toggleButton = document.querySelector(".toggle-button");
     this.toggleCircle = document.querySelector(".toggle-circle");
 
-    document.body.classList.remove("light-theme", "dark-theme");
-    document.body.classList.add(`${this.theme}-theme`);
-    this.toggleCircle.classList.toggle("slide", this.theme === "dark");
-
     this.setEventListener();
-
-    this.emit("switch", this.theme);
   }
 
   setEventListener() {
